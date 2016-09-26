2d335d330d10473b5e39604d7be71e0f6dfb304f
m-lima <marcelowind@gmail.com>
2016-09-27 00:39:13 +0200

    Sun implemented; Could be better tho

--- assets/shaders/cube.fs.glsl
+++ assets/shaders/cube.fs.glsl
@@ -1,13 +1,20 @@
 #version 430
 
 in vec3 vertexOut;
-in vec3 fNormal;
+in vec3 fNormal; //Already normalized
 out vec4 fragColor;
 
-//Assumed to be normalized to avoid per-pixel normalization
-uniform vec3 lightPos;
+uniform vec3 lightPos; //Assumed to be normalized to avoid per-pixel normalization
+uniform mat4 view;
+
+const vec3 sunPos = mat3((inverse(view))) * normalize(vec3(1.0)); //Calculation performed only once per frame
 
 void main() {       
-	float bright = dot(normalize(fNormal), lightPos);
-    fragColor = vec4(bright, bright, bright, 1.0);
+	float flashlight = dot(fNormal, lightPos) * 0.5;
+	vec3 flashlightColor = vec3(flashlight);
+ 
+	float sunLight = dot(fNormal, sunPos);
+	vec3 sunColor = vec3(sunLight);
+
+    fragColor = vec4(sunColor + flashlightColor, 1.0);
 }
