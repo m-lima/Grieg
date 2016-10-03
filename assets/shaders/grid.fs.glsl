b3786a17b2be47ac8b2bca669421ec5ce7e647f6
Zohar Malamant <dotfloat@gmail.com>
2016-10-03 19:52:17 +0200

    Improve the grid shader

--- assets/shaders/grid.fs.glsl
+++ assets/shaders/grid.fs.glsl
@@ -6,16 +6,13 @@ out vec4 FragColor;
 const vec4 gridColor = vec4(1.0, 0.0, 1.0, 0.2);
 const vec4 voidColor = vec4(0.0, 0.0, 0.0, 0.0);
 const float interval = 50.0;
-const float width = 1.0;
-
-bool check(float f)
-{
-    return int(mod(f / width, interval / width)) == 0;
-}
 
 void main()
 {
-    float line = (check(fPosition.x) || check(fPosition.z)) ? 1.0 : 0.0;
+    // Stolen from http://madebyevan.com/shaders/grid/
+    vec2 coord = fPosition.xz / interval;
+    vec2 grid = abs(fract(coord - 0.5) - 0.5) / fwidth(coord);
+    float line = min(grid.x, grid.y);
 
-    FragColor = mix(voidColor, gridColor, line);
+    FragColor = mix(voidColor, gridColor, 1.0 - min(line, 1.0));
 }
