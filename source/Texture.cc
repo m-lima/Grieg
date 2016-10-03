05d839ab64a3c00552a56c0f67b59cc307464a74
Zohar Malamant <dotfloat@gmail.com>
2016-10-03 15:27:20 +0200

    Generate mipmaps

--- source/Texture.cc
+++ source/Texture.cc
@@ -2,15 +2,8 @@
 
 void Texture::init()
 {
-    if (!mTexture) {
+    if (!mTexture)
         glGenTextures(1, &mTexture);
-        glActiveTexture(GL_TEXTURE0 + mTextureOffset);
-        glBindTexture(GL_TEXTURE_2D, mTexture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    }
 }
 
 void Texture::load(const std::string & name)
@@ -28,15 +21,23 @@ void Texture::load(const std::string & name)
 
     init();
 
-    glTexImage2D(GL_TEXTURE_2D,
-                 0,
-                 GL_RGB8,
-                 surface->w,
-                 surface->h,
-                 0,
-                 GL_RGB,
-                 GL_UNSIGNED_BYTE,
-                 surface->pixels);
+    glActiveTexture(GL_TEXTURE0 + mTextureOffset);
+    glBindTexture(GL_TEXTURE_2D, mTexture);
+    glTexStorage2D(GL_TEXTURE_2D, 4, GL_RGB8, surface->w, surface->h);
+    glTexSubImage2D(GL_TEXTURE_2D,
+                    0, /* mipmap level */
+                    0, /* x-offset */
+                    0, /* y-offset */
+                    surface->w,
+                    surface->h,
+                    GL_RGB,
+                    GL_UNSIGNED_BYTE,
+                    surface->pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glGenerateMipmap(GL_TEXTURE_2D);
 
     SDL_FreeSurface(surface);
 }
