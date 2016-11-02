4fb7c5cbffdb8f0018419095d7b8262eb0e0d11e
m-lima <marcelowind@gmail.com>
2016-11-02 18:26:52 +0100

    Forcing high performance GPU programmatically

--- source/main.cc
+++ source/main.cc
@@ -3,27 +3,36 @@
 #include "Sdl.hh"
 #include "Renderer.hh"
 
+// Force high performance GPU
+extern "C" {
+  // NVidia
+  __declspec(dllexport) DWORD NvOptimusEnablement = 1;
+
+  // AMD
+  __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 std::string readFileContents(const std::string &file)
 {
-    std::ifstream fh(file, std::ios::binary);
-    if (!fh.is_open())
-        throw std::runtime_error(format("Couldn't open file {}", file));
+  std::ifstream fh(file, std::ios::binary);
+  if (!fh.is_open())
+    throw std::runtime_error(format("Couldn't open file {}", file));
 
-    std::string buf;
+  std::string buf;
 
-    fh.seekg(0, std::ios::end);
-    auto length = fh.tellg();
-    buf.reserve(static_cast<size_t>(length));
-    fh.seekg(0, std::ios::beg);
-    buf.assign(std::istreambuf_iterator<char>(fh), std::istreambuf_iterator<char>());
+  fh.seekg(0, std::ios::end);
+  auto length = fh.tellg();
+  buf.reserve(static_cast<size_t>(length));
+  fh.seekg(0, std::ios::beg);
+  buf.assign(std::istreambuf_iterator<char>(fh), std::istreambuf_iterator<char>());
 
-    return buf;
+  return buf;
 }
 
 int main()
 {
-    Sdl::setGlInit(Renderer::init);
-    Sdl::setGlResize(Renderer::resize);
-    Sdl::setGlDisplay(Renderer::draw);
-    Sdl::mainLoop();
+  Sdl::setGlInit(Renderer::init);
+  Sdl::setGlResize(Renderer::resize);
+  Sdl::setGlDisplay(Renderer::draw);
+  Sdl::mainLoop();
 }
